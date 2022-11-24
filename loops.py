5de528d965e860630eddf534bbb77a93d89a3798
wiki112 <Viktorian.ww@gmail.com>
2022-11-24 19:57:39 +0100

    init commit

---
+++ loops.py
@@ -0,0 +1,18 @@
+x=1
+
+warunek=True
+while warunek:
+    print(F"{x}")
+    while x >5 and x%5==0:
+        print("duża liczba!")
+        break
+    x+=1
+    if x > 25:
+        break
+    pass
+
+
+
+
+
+
